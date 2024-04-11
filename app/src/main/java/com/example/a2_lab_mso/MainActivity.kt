9a5cb46e830279eea7ab857bf71b709d44fb793c
Filip <hagiel120@gmail.com>
2024-04-11 03:11:48 +0200

    Added a new button and an ImageView to main activity. Pressing the button starts the system camera app. After a picture is taken, the ImageView display its thumbnail.

--- app/src/main/java/com/example/a2_lab_mso/MainActivity.kt
+++ app/src/main/java/com/example/a2_lab_mso/MainActivity.kt
@@ -1,36 +1,64 @@
 package com.example.a2_lab_mso
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "UseSwitchCompatOrMaterialCode")
+    @SuppressLint("MissingInflatedId", "UseSwitchCompatOrMaterialCode", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val captureImageView = findViewById<ImageView>(R.id.captureImageView)
+
+        val startForResultActivity = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val thumbnail: Bitmap? = data?.extras?.get("data") as? Bitmap
+                thumbnail?.let {
+                    captureImageView.setImageBitmap(thumbnail)
+                }
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val startButton = findViewById<Button>(R.id.button2)
+        val buttonDraw = findViewById<Button>(R.id.button2)
         val sw1 = findViewById<Switch>(R.id.switch2)
-        startButton.setOnClickListener {
+        val buttonCamera = findViewById<Button>(R.id.button3)
+        buttonDraw.setOnClickListener {
             val secondActivityIntent =
                 Intent(this, SecondActivity::class.java)
             secondActivityIntent.putExtra("SwitchStatus", sw1.isChecked)
             startActivity(secondActivityIntent)
         }
+        buttonCamera.setOnClickListener {
+            val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startForResultActivity.launch(camera)
+        }
+
     }
 }
